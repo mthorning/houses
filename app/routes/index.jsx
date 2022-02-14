4b9c14bbfdc70863897c4f0b6fd88d6c4bcb1401
Matthew Thorning <matthew@ometria.com>
2022-02-14 17:09:44 +0000

    fix spacing

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,12 @@
 const Iframe = ({ src }) => (
   <iframe
     {...{ src }}
-    style={{ display: "block", width: "100%", height: "400px", margin: "16px" }}
+    style={{
+      display: "block",
+      width: "100%",
+      height: "400px",
+      margin: "0 auto 16px",
+    }}
   />
 );
 
@@ -10,7 +15,7 @@ export default function Index() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <ul>
         <Iframe src="/zoopla" />
-        <Iframe src="https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E1365&minBedrooms=2&maxPrice=550000&minPrice=300000&radius=3.0&sortType=6&propertyTypes=detached%2Csemi-detached%2Cterraced&primaryDisplayPropertyType=houses&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=" />
+        <Iframe src="/rightmove" />
       </ul>
     </div>
   );
